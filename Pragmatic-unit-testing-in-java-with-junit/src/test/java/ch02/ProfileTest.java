9e4ad2350182d0ce7c987b8b172489dc29830445
lenkim <joenggyu0@gmail.com>
2021-01-31 17:11:09 +0900

    add Profile guide code

--- Pragmatic-unit-testing-in-java-with-junit/src/test/java/ch02/ProfileTest.java
+++ Pragmatic-unit-testing-in-java-with-junit/src/test/java/ch02/ProfileTest.java
@@ -7,16 +7,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class ProfileTest {
 
     @Test
-    void test() {
+    void matchAnswerFalseWhenMustMatchCriteriaNotMet() {
         Profile profile = new Profile("Bull Hockey, Inc.");
-
         Question question = new BooleanQuestion(1, "Got bonuses?");
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+
         Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
 
         criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertFalse(matches);
     }
 
+    @Test
+    void matchAnswersTrueForAnyDontCareCriteria() {
+        Profile profile = new Profile("Bull Hockey, Inc.");
+        Question question = new BooleanQuestion(1, "Got bonuses?");
+
+        Answer profileAnswer = new Answer(question, Bool.FALSE);
+        profile.add(profileAnswer);
+        Criteria criteria = new Criteria();
+        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
+
+        criteria.add(criterion);
+
+        boolean matches = profile.matches(criteria);
+
+        assertTrue(matches);
+
+    }
 
 }
