72da0c2beab28f95fed748afca088661bc591280
LenKIM <joenggyu0@gmail.com>
2022-04-05 23:31:03 +0900

    impl domain

---
+++ clean-architecture/src/main/java/io/reflectoring/buckpal/MoneyTransferProperties.java
@@ -0,0 +1,8 @@
+package io.reflectoring.buckpal;
+
+import io.reflectoring.buckpal.account.domain.Money;
+
+public class MoneyTransferProperties {
+    public MoneyTransferProperties(Money of) {
+    }
+}
